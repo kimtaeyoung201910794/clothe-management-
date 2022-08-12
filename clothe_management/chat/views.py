3f98f541c5aea687c385ed8efe386e6b1614578d
jomulagy688@gmail.com <jomulagy688@gmail.com>
2022-08-13 02:15:32 +0900

    채팅관련 기능 추가
    1. 채팅목록 및 채팅 내용
    2.메세지 보내기

--- clothe_management/chat/views.py
+++ clothe_management/chat/views.py
@@ -1,5 +1,50 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from .forms import MessageForm
+from account.models import myUser
+from .models import ChatRoom, Message
 # Create your views here.
-def chatting(request):
-    return render(request, 'chat/main.html')
+def chatting(request,chatroom_id):
+    chatrooms = [chatroom for chatroom in ChatRoom.objects.filter(users__id = request.user.id).all()]
+    context = {
+        'chatrooms' : chatrooms,
+        }
+    if chatroom_id != '0':
+        chats = Message.objects.filter(chatroom_id = chatroom_id)
+        for chat in chats:
+            if chat.read == False:
+                chat.read = True
+                chat.save()
+        user = ChatRoom.objects.get(id = chatroom_id).users.exclude(id = request.user.id)
+        detail = True
+        context['chat_detail']=chats
+        context['user_id']=user[0].id
+        
+    else:
+        detail = False
+    context['detail']=detail
+    
+    return render(request, 'chat/main.html',context)
+
+def send_msg(request):
+    if request.method == "POST":
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            chatroom = ChatRoom.objects.filter(users__id = request.user.id and request.POST.get('user_id')).first()
+            if chatroom==None:
+                chatroom = ChatRoom()
+                if request.POST.get('annonymous')==True:
+                    chatroom.annonymous=True
+                chatroom.save()
+                chatroom.users.add(request.user)
+                chatroom.users.add(myUser.objects.get(id = request.POST.get('user_id')))
+                chatroom.save()
+            
+            new_msg = form.save(commit=False)
+            new_msg.user = myUser.objects.get(id = request.user.id)
+            new_msg.chatroom = chatroom
+            new_msg.save()
+            
+        return redirect(request.POST.get('prv_path'))
+    
+def msg_detail(request):
+    return render(request, "chat/list.html")
