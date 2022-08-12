dc3fb0303493d225e7a257772991e3bbf1b54b0b
jomulagy688@gmail.com <jomulagy688@gmail.com>
2022-08-13 02:16:05 +0900

    채팅 모델 함수 추가
    메세지 보낸 시간 정해진 형식으로 출력하기

--- clothe_management/chat/models.py
+++ clothe_management/chat/models.py
@@ -1,9 +1,35 @@
 from django.db import models
+from datetime import datetime,timedelta
 
 class ChatRoom(models.Model):
-    users = models.ForeignKey('account.myUser',on_delete=models.CASCADE)
+    users = models.ManyToManyField('account.myUser')
+    annonymous = models.BooleanField(default = False)
 
 class Message(models.Model):
-    users = models.OneToOneField('account.myUser',on_delete=models.CASCADE)
+    user = models.ForeignKey('account.myUser',on_delete=models.CASCADE)
     time = models.DateTimeField(auto_now_add=True)
     text = models.TextField()
+    chatroom = models.ForeignKey(ChatRoom,on_delete=models.CASCADE)
+    read = models.BooleanField(null=False,default = False)
+
+    @property
+    def get_time(self):
+        time = datetime.now() - self.time
+        result = ''
+        if len(str(self.time.month))==1:
+            result+='0'
+        result+=str(self.time.month)+'/'
+        if len(str(self.time.day))==1:
+            result+='0'
+        result+=str(self.time.day)+' '
+        if len(str(self.time.hour))==1:
+            result+='0'
+        result+=str(self.time.hour)+":"
+        if len(str(self.time.minute))==1:
+            result+='0'
+        result+=str(self.time.minute)
+
+        if time < timedelta(days=365):
+            return result
+        else:
+            return str(self.time.year)+result
